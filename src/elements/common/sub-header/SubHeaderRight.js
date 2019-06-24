66d77cef989da2c84aa0ce774f021915a8c76486
rickstefanik <rstefanik@box.com>
2019-06-24 15:35:03 -0700

    feat: added toggle between list and grid view

--- src/elements/common/sub-header/SubHeaderRight.js
+++ src/elements/common/sub-header/SubHeaderRight.js
@@ -7,35 +7,47 @@
 import React from 'react';
 import Sort from './Sort';
 import Add from './Add';
+import GridViewButton from './GridViewButton';
 import { VIEW_FOLDER } from '../../../constants';
+import { FeatureFlag } from '../feature-checking';
 import './SubHeaderRight.scss';
 
 type Props = {
     canCreateNewFolder: boolean,
     canUpload: boolean,
+    columnCount: number,
     currentCollection: Collection,
+    isGridView?: boolean,
     onCreate: Function,
+    onGridViewSwitch?: Function,
     onSortChange: Function,
     onUpload: Function,
     view: View,
 };
 
 const SubHeaderRight = ({
-    view,
-    onUpload,
-    onCreate,
-    canUpload,
     canCreateNewFolder,
+    canUpload,
     currentCollection,
+    isGridView,
+    onCreate,
+    onGridViewSwitch,
     onSortChange,
+    onUpload,
+    view,
 }: Props) => {
     const { sortBy, sortDirection, items = [] }: Collection = currentCollection;
     const isFolder: boolean = view === VIEW_FOLDER;
     const showSort: boolean = isFolder && items.length > 0;
     const showAdd: boolean = (!!canUpload || !!canCreateNewFolder) && isFolder;
+    const showGridButton: boolean = isFolder && items.length > 0;
 
     return (
         <div className="be-sub-header-right">
+            <FeatureFlag feature="contentExplorer.gridView.buttonVisible">
+                {showGridButton && <GridViewButton isGridView={isGridView} onClick={onGridViewSwitch} />}
+            </FeatureFlag>
+
             {showSort && !!sortBy && !!sortDirection && (
                 <Sort onSortChange={onSortChange} sortBy={sortBy} sortDirection={sortDirection} />
             )}
--- src/elements/content-explorer/Content.js
+++ src/elements/content-explorer/Content.js
@@ -31,6 +31,7 @@ type Props = {
     canShare: boolean,
     currentCollection: Collection,
     focusedRow: number,
+    isGridView?: boolean,
     isMedium: boolean,
     isSmall: boolean,
     isTouch: boolean,
@@ -49,62 +50,73 @@ type Props = {
 };
 
 const Content = ({
-    view,
-    isSmall,
-    isMedium,
-    isTouch,
-    rootId,
-    rootElement,
-    currentCollection,
-    tableRef,
-    focusedRow,
-    canDownload,
     canDelete,
+    canDownload,
+    canPreview,
     canRename,
     canShare,
-    canPreview,
+    currentCollection,
+    focusedRow,
+    isGridView = false,
+    isMedium,
+    isSmall,
+    isTouch,
     onItemClick,
-    onItemSelect,
     onItemDelete,
     onItemDownload,
-    onItemRename,
-    onItemShare,
     onItemPreview,
+    onItemRename,
+    onItemSelect,
+    onItemShare,
     onSortChange,
-}: Props) => (
-    <div className="bce-content">
-        {view === VIEW_ERROR || view === VIEW_SELECTED ? null : (
-            <ProgressBar percent={currentCollection.percentLoaded} />
-        )}
-        {isEmpty(view, currentCollection) ? (
-            <EmptyState view={view} isLoading={currentCollection.percentLoaded !== 100} />
-        ) : (
-            <ItemList
-                view={view}
-                isSmall={isSmall}
-                isMedium={isMedium}
-                isTouch={isTouch}
-                rootId={rootId}
-                rootElement={rootElement}
-                focusedRow={focusedRow}
-                currentCollection={currentCollection}
-                tableRef={tableRef}
-                canShare={canShare}
-                canPreview={canPreview}
-                canDelete={canDelete}
-                canRename={canRename}
-                canDownload={canDownload}
-                onItemClick={onItemClick}
-                onItemSelect={onItemSelect}
-                onItemDelete={onItemDelete}
-                onItemDownload={onItemDownload}
-                onItemRename={onItemRename}
-                onItemShare={onItemShare}
-                onItemPreview={onItemPreview}
-                onSortChange={onSortChange}
-            />
-        )}
-    </div>
-);
+    rootElement,
+    rootId,
+    tableRef,
+    view,
+}: Props) => {
+    const listView = (
+        <ItemList
+            view={view}
+            isSmall={isSmall}
+            isMedium={isMedium}
+            isTouch={isTouch}
+            rootId={rootId}
+            rootElement={rootElement}
+            focusedRow={focusedRow}
+            currentCollection={currentCollection}
+            tableRef={tableRef}
+            canShare={canShare}
+            canPreview={canPreview}
+            canDelete={canDelete}
+            canRename={canRename}
+            canDownload={canDownload}
+            onItemClick={onItemClick}
+            onItemSelect={onItemSelect}
+            onItemDelete={onItemDelete}
+            onItemDownload={onItemDownload}
+            onItemRename={onItemRename}
+            onItemShare={onItemShare}
+            onItemPreview={onItemPreview}
+            onSortChange={onSortChange}
+        />
+    );
+
+    const gridView = <div>Placeholder for grid view</div>;
+
+    const content = isGridView ? gridView : listView;
+
+    return (
+        <div className="bce-content">
+            {view === VIEW_ERROR || view === VIEW_SELECTED ? null : (
+                <ProgressBar percent={currentCollection.percentLoaded} />
+            )}
+            {isEmpty(view, currentCollection) ? (
+                <EmptyState view={view} isLoading={currentCollection.percentLoaded !== 100} />
+            ) : (
+                content
+            )}
+        </div>
+    );
+};
 
 export default Content;
